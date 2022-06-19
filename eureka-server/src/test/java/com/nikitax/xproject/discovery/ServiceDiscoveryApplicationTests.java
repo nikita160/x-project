69888bcfdbd3abcb6944f2a8460fccdd3bc88282
Nikita <nikitax160@yandex.ru>
2022-06-19 22:55:11 +0300

    eureka server added

---
+++ eureka-server/src/test/java/com/nikitax/xproject/discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikitax.xproject.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
